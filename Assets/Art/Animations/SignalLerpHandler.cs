6aef91abce586960b2fdaedaf6abb37497e9a9f7
Ammar Keshta <Ammarkeshta650@gmail.com>
2025-10-22 20:56:13 +0300

    data extra

--- Assets/Art/Animations/SignalLerpHandler.cs
+++ Assets/Art/Animations/SignalLerpHandler.cs
@@ -4,13 +4,20 @@
 using System;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class SignalLerpHandler : MonoBehaviour
 {
+
+    public Volume globalVolume; // اسحب هنا الـ Global Volume من الـ Inspector
+    private ColorAdjustments colorAdjustments;
     public float st = 0f;
     public float et = 0f;
     public float ct = 0f;
     public float pt = 0f;
+
+    public float pt2 = 0f;
 
     public float lerpDuration = 2f;
     public float lerpDurationC = 3f;
@@ -20,19 +27,21 @@ public class SignalLerpHandler : MonoBehaviour
     private bool isLerping2 = false;
     private bool isLerpingC = false;
     private bool isLerpingP = false;
+    private bool isLerpingP2 = false;
 
     private float lerpStartTime;
     private float lerpStartTime2;
     private float lerpStartTimeC;
     private float lerpStartTimeP;
+    private float lerpStartTimeP2;
 
-    public Material material1; 
+    public Material material1;
 
-    public Material material2; 
+    public Material material2;
 
-    public Material materialC; 
+    public Material materialC;
 
-    public Material materialP; 
+    public Material materialP;
 
     public string parameterReference = "_Diss"; // Reference name of the Shader Graph parameter
 
@@ -57,13 +66,13 @@ public class SignalLerpHandler : MonoBehaviour
 
         if (materialC != null)
         {
-  
+
             materialC.SetFloat(parameterReferenceC, ct);
         }
 
         if (materialP != null)
         {
-  
+
             materialP.SetFloat(parameterReferenceP, pt);
         }
 
@@ -112,28 +121,51 @@ public class SignalLerpHandler : MonoBehaviour
             }
         }
 
-                if (isLerpingP)
+        if (isLerpingP)
         {
             // Calculate the lerp progress
             float elapsedTimeP = Time.time - lerpStartTimeP;
             pt = Mathf.Lerp(0f, 1f, elapsedTimeP / lerpDurationP);
-
+            Invoke("DoSomething", 4f); // بعد 3 ثواني
 
             // Stop lerping when the duration is reached
             if (elapsedTimeP >= lerpDurationP)
             {
-                ct = 1f; // Ensure it ends exactly at 1
+                pt = 1f; // Ensure it ends exactly at 1
+                isLerpingP = false;
+            }
+        }
+
+        if (isLerpingP2)
+        {
+            // Calculate the lerp progress
+            float elapsedTimeP2 = Time.time - lerpStartTimeP2;
+            pt = Mathf.Lerp(1f, 0f, elapsedTimeP2 / (lerpDurationP*0.5f));
+
+            // Stop lerping when the duration is reached
+            if (elapsedTimeP2 >= (lerpDurationP*0.5f))
+            {
+                pt = 0f; // Ensure it ends exactly at 1
                 isLerpingP = false;
             }
         }
 
     }
 
-    // This method will be called when the signal is triggered
+
+    void DoSomething()
+    {
+        isLerpingP2 = true;
+        lerpStartTimeP2 = Time.time;
+    }
+
+
     public void OnSignalReceived()
     {
         isLerping = true;
         lerpStartTime = Time.time;
+
+
     }
 
     public void OnSignal2Received()
@@ -154,5 +186,21 @@ public class SignalLerpHandler : MonoBehaviour
         lerpStartTimeP = Time.time;
     }
 
+
+    public void SetExposure()
+    {
+
+        // نحاول نجيب الـ ColorAdjustments من الـ VolumeProfile
+        if (globalVolume.profile.TryGet<ColorAdjustments>(out colorAdjustments))
+        {
+            // مثال: نخلي الـ Post Exposure يساوي 1
+            colorAdjustments.postExposure.value = 30f;
+        }
+        else
+        {
+            Debug.LogWarning("Color Adjustments مش موجودة في الـ Global Volume!");
+        }
+
+    }
 } 
 
